8fa4010933f24e7fcc6997f9fe18c81e80c717e2
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-09-08 10:13:29 +0300

    Task1

---
+++ Homework5_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int [] array = new int [6];
+int countEven = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = new Random().Next(100,999);
+    if(array[i]%2 ==0)
+    {
+        countEven++;
+    }
+}
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine("Number of even numbers: "+ countEven);
