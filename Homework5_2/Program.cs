b80b5478c572a18a794c8f283d50b990c37a10ba
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-09-08 10:23:35 +0300

    Task2

---
+++ Homework5_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int [] array = new int [6];
+int oddIndex = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100,100);
+    if(i%2 == 1)
+    {
+        oddIndex+=array[i];
+    }
+}
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine("Sum of elements with an odd index is " + oddIndex);
